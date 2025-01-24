210d3ec9d1dae7ed5b9ec7ff244df81f9fd6ccbf
hatimaderras <hatem.aderras@gmail.com>
2025-01-24 12:59:20 +0100

    adding defrente image for defrente tempuratures

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const apiKey = "8284a3439165e88b685562c873e08637";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+const weathericon=document.querySelector(".weather-icon")
 
 const searchBox = document.querySelector(".search-bar"); 
 const searchBtn = document.querySelector(".btn-primary");
@@ -13,6 +14,23 @@ async function checkWeather(city) {
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds"){
+        weathericon.src = "assests/clouds.png";
+    }
+    else if (data.weather[0].main == "Clear"){
+        weathericon.src = "assests/clear.png";
+    }
+     else if (data.weather[0].main == "Rain"){
+        weathericon.src = "assests/clear.png";
+    }
+    else if (data.weather[0].main == "Drizzle"){
+        weathericon.src = "assests/drizzle.png";
+    }
+    else if (data.weather[0].main == "Mist"){
+        weathericon.src = "assests/mist.png";
+    }
+
 }   
 
 searchBtn.addEventListener("click", ()=>{
