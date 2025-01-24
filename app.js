929578cdf89e634b5632b63a51e1e99deb40af0b
hatimaderras <hatem.aderras@gmail.com>
2025-01-24 10:56:05 +0100

    fixing an ereur in selecting by class in button and search

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const apiKey = "8284a3439165e88b685562c873e08637";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const searchBox=document.querySelector(".d-flex align-items-center input");
-const searchBtn=document.querySelector(".d-flex align-items-center button");
 
-
+const searchBox = document.querySelector(".search-bar"); 
+const searchBtn = document.querySelector(".btn-primary");
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
-
+    
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+}   
 
-}
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
 })
+
 
