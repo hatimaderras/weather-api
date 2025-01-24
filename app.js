a4cf0134be2159daf69624f6a05d5684312257d5
hatimaderras <hatem.aderras@gmail.com>
2025-01-24 10:48:03 +0100

    api request using fetch

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const apiKey = "8284a3439165e88b685562c873e08637";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+}
