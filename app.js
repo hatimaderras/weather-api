25e60f00a85fe41d3461f4248f820c100a73c24b
hatimaderras <hatem.aderras@gmail.com>
2025-01-24 10:49:16 +0100

    selecting each element by dom

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
 }
